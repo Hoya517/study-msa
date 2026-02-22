1f0675b92d533e2589d9a43be297f58480e4b8fb
hoya <sv05159@naver.com>
2026-02-22 22:07:06 +0900

    integrate distributed tracing with Micrometer and Zipkin

--- com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
+++ com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -2,6 +2,7 @@ package com.spring_cloud.eureka.client.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,8 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @GetMapping("/order")
-    public String getOrder() {
-        return "Order detail";
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String orderId) {
+        return orderService.getOrder(orderId);
     }
 }
